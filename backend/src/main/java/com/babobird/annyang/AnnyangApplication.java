868056b06f9fea2c3f05834e5be134234972614c
sebae <morendo90@gmail.com>
2024-10-27 22:21:35 +0900

    Add backend and frontend folders

---
+++ backend/src/main/java/com/babobird/annyang/AnnyangApplication.java
@@ -0,0 +1,13 @@
+package com.babobird.annyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnyangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnyangApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/babobird/annyang/AnnyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babobird.annyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnyangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
